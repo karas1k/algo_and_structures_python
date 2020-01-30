8141ab57f493410854349d3804cc102681c25de4
karas1k <shtraz07@gmail.com>
2020-01-30 13:14:16 +0300

    Update Home work lesson 7 from Ushakov M.V.

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,131 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+from random import randint
+import timeit
+
+
+def smart_bubble(numbers_list):
+    """bubble_sort_optimization"""
+    for i in range(len(numbers_list) - 1, 0, -1):
+        flag = True
+        for j in range(i):
+            if numbers_list[j] < numbers_list[j + 1]:
+                numbers_list[j], numbers_list[j + \
+                    1] = numbers_list[j + 1], numbers_list[j]
+                flag = False
+        if flag:
+            break
+    return numbers_list
+
+
+def no_smart_bubble(numbers_list):
+    """bubble_sort_NOT_optimization"""
+    for i in range(len(numbers_list) - 1, 0, -1):
+        for j in range(i):
+            if numbers_list[j] < numbers_list[j + 1]:
+                numbers_list[j], numbers_list[j +
+                                              1] = numbers_list[j + 1], numbers_list[j]
+
+    return numbers_list
+
+
+def standart_sort_func(numbers_list):
+    """Стандартная функция сортировки"""
+    numbers_list.sort(reverse=True)
+    return numbers_list
+
+# Замеры 10
+NUMBERS_LIST = [randint(-100, 100) for i in range(10)]
+print(NUMBERS_LIST)
+print(smart_bubble(NUMBERS_LIST))
+
+TIME_SMART_BUBBLE = timeit.timeit(
+    "smart_bubble(NUMBERS_LIST)",
+    setup="from __main__ import smart_bubble, NUMBERS_LIST",
+    number=1000)
+
+TIME_NOT_SMART_BUBBLE = timeit.timeit(
+    "no_smart_bubble(NUMBERS_LIST)",
+    setup="from __main__ import no_smart_bubble, NUMBERS_LIST",
+    number=1000)
+
+TIME_SORTED_FUNC = timeit.timeit(
+    "standart_sort_func(NUMBERS_LIST)",
+    setup="from __main__ import standart_sort_func, NUMBERS_LIST",
+    number=1000)
+
+print(f'Время на сортировку "умным пузырьком": {TIME_SMART_BUBBLE}')
+print(
+    f'Время на сортировку НЕ "умным пузырьком": {TIME_NOT_SMART_BUBBLE}')
+print(
+    f'Время на сортировку функцией sort(): {TIME_SORTED_FUNC}\n{"*" * 70}\n')
+
+# Замеры 100
+
+NUMBERS_LIST = [randint(-100, 100) for i in range(100)]
+print(NUMBERS_LIST)
+print(smart_bubble(NUMBERS_LIST))
+
+TIME_SMART_BUBBLE = timeit.timeit(
+    "smart_bubble(NUMBERS_LIST)",
+    setup="from __main__ import smart_bubble, NUMBERS_LIST",
+    number=1000)
+
+TIME_NOT_SMART_BUBBLE = timeit.timeit(
+    "no_smart_bubble(NUMBERS_LIST)",
+    setup="from __main__ import no_smart_bubble, NUMBERS_LIST",
+    number=1000)
+
+TIME_SORTED_FUNC = timeit.timeit(
+    "standart_sort_func(NUMBERS_LIST)",
+    setup="from __main__ import standart_sort_func, NUMBERS_LIST",
+    number=1000)
+
+print(f'Время на сортировку "умным пузырьком": {TIME_SMART_BUBBLE}')
+print(
+    f'Время на сортировку НЕ "умным пузырьком": {TIME_NOT_SMART_BUBBLE}')
+print(
+    f'Время на сортировку функцией sort(): {TIME_SORTED_FUNC}\n{"*" * 70}\n')
+
+# Замеры 1000
+
+NUMBERS_LIST = [randint(-100, 100) for i in range(1000)]
+print(NUMBERS_LIST)
+print(smart_bubble(NUMBERS_LIST))
+
+TIME_SMART_BUBBLE = timeit.timeit(
+    "smart_bubble(NUMBERS_LIST)",
+    setup="from __main__ import smart_bubble, NUMBERS_LIST",
+    number=1000)
+
+TIME_NOT_SMART_BUBBLE = timeit.timeit(
+    "no_smart_bubble(NUMBERS_LIST)",
+    setup="from __main__ import no_smart_bubble, NUMBERS_LIST",
+    number=1000)
+
+TIME_SORTED_FUNC = timeit.timeit(
+    "standart_sort_func(NUMBERS_LIST)",
+    setup="from __main__ import standart_sort_func, NUMBERS_LIST",
+    number=1000)
+
+print(f'Время на сортировку "умным пузырьком": {TIME_SMART_BUBBLE}')
+print(
+    f'Время на сортировку НЕ "умным пузырьком": {TIME_NOT_SMART_BUBBLE}')
+print(
+    f'Время на сортировку функцией sort(): {TIME_SORTED_FUNC}\n{"*" * 70}\n')
+
+"""
+Замеры 10:
+    Время на сортировку "умным пузырьком": 0.0024717999999999997
+    Время на сортировку НЕ "умным пузырьком": 0.011228600000000002
+    Время на сортировку функцией sort(): 0.00035389999999999727
+Замеры 100:
+    Время на сортировку "умным пузырьком": 0.016413999999999998
+    Время на сортировку НЕ "умным пузырьком": 0.8214319
+    Время на сортировку функцией sort(): 0.0010989999999999611
+Замеры 1000:
+    Время на сортировку "умным пузырьком": 0.18817909999999993
+    Время на сортировку НЕ "умным пузырьком": 87.9788297
+    Время на сортировку функцией sort(): 0.008549799999997276
+"""
--- Lesson_7/task_2.py
+++ Lesson_7/task_2.py
@@ -5,5 +5,70 @@
 """
 
 from random import uniform
-NUMBERS_LIST = [uniform(-100, 100) for i in range(10)]
-print(NUMBERS_LIST)
+from timeit import timeit
+
+
+def merge_sort(numbers_list):
+    """Сортировка слияением"""
+    if len(numbers_list) > 1:
+        center = len(numbers_list) // 2
+        left = numbers_list[:center]
+        right = numbers_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        # перестали делить
+        # выполняем слияние
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                numbers_list[k] = left[i]
+                i += 1
+            else:
+                numbers_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            numbers_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            numbers_list[k] = right[j]
+            j += 1
+            k += 1
+    return numbers_list
+
+
+# замеры 10
+NUMBERS_LIST = [round(uniform(0, 50), 2) for i in range(10)]
+print(f"Исходный массив 10:\n{NUMBERS_LIST}\n")
+print(f"Отсортированный массив 10:\n{merge_sort(NUMBERS_LIST)}\n")
+TIME = timeit(
+    "merge_sort(NUMBERS_LIST)",
+    setup="from __main__ import merge_sort, NUMBERS_LIST",
+    number=1000)
+print(f'Время: {TIME}')
+
+# замеры 100
+NUMBERS_LIST = [round(uniform(0, 50), 2) for i in range(100)]
+print(f"Исходный массив 100:\n{NUMBERS_LIST}\n")
+print(f"Отсортированный массив 100:\n{merge_sort(NUMBERS_LIST)}\n")
+TIME = timeit(
+    "merge_sort(NUMBERS_LIST)",
+    setup="from __main__ import merge_sort, NUMBERS_LIST",
+    number=1000)
+print(f'Время: {TIME}')
+
+# замеры 1000
+NUMBERS_LIST = [round(uniform(0, 50), 2) for i in range(1000)]
+print(f"Исходный массив 1000:\n{NUMBERS_LIST}\n")
+print(f"Отсортированный массив 1000:\n{merge_sort(NUMBERS_LIST)}\n")
+TIME = timeit(
+    "merge_sort(NUMBERS_LIST)",
+    setup="from __main__ import merge_sort, NUMBERS_LIST",
+    number=1000)
+print(f'Время: {TIME}')
