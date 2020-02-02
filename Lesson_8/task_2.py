b245a6643587461c0fbc19b7d48df86178a8d2e8
karas1k <shtraz07@gmail.com>
2020-02-02 19:14:42 +0300

    lesson_8 task 2

--- Lesson_8/task_2.py
+++ Lesson_8/task_2.py
@@ -15,5 +15,4 @@ for i in range(len(MY_STRING)):
     for j in range(len(MY_STRING), i, -1):
         hash_string = hashlib.sha1(MY_STRING[i:j].encode('utf-8')).hexdigest()
         SUMM.add(hash_string)
-
 print(f"В строке {MY_STRING} {len(SUMM) - 1} уникальных подстрок")
