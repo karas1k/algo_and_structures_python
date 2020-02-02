34a2360ceeb6513e6517ccdeebb298829bef33b1
karas1k <45075660+karas1k@users.noreply.github.com>
2020-02-02 19:20:32 +0300

    Update 2.py

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,15 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import hashlib
+
+MY_STRING = input('Введите строку, состоящую из латинских букв: ')
+MY_STRING = MY_STRING.lower()
+# print(MY_STRING)
+SUMM = set()
+
+for i in range(len(MY_STRING)):
+    for j in range(len(MY_STRING), i, -1):
+        hash_string = hashlib.sha1(MY_STRING[i:j].encode('utf-8')).hexdigest()
+        SUMM.add(hash_string)
+print(f"В строке {MY_STRING} {len(SUMM) - 1} уникальных подстрок")
